5ffb1373f56477b19aee6a97e0625289ec725b8e
martinmanuel9 <martinmlopez@arizona.edu>
2023-09-28 07:03:24 -0700

    added spark docker container

--- models/dis/devices/fridgeSim.py
+++ models/dis/devices/fridgeSim.py
@@ -59,7 +59,7 @@ class FridgeSim:
 
         if self.transmission == 'kafka' or self.transmission == 'kafka_pdu':
             # Kafka Producer
-            self.KAFKA_TOPIC = 'dis'
+            self.KAFKA_TOPIC = 'fridge'
             self.producer = kp.KafkaProducer('localhost:9092', self.KAFKA_TOPIC)
         
 
