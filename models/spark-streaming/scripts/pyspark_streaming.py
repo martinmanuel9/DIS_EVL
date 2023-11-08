c6c06dbf79f635a56c6ed16aaf3b35d470bf0820
martinmanuel9 <martinmlopez@arizona.edu>
2023-11-07 22:56:33 -0700

    updates to pyspark for multiple streams

--- models/spark-streaming/scripts/pyspark_streaming.py
+++ models/spark-streaming/scripts/pyspark_streaming.py
@@ -67,199 +67,206 @@ class SparkStructuredStreaming:
 
         # -----------------------------------------------
         # Process data for the "fridge" topic
-        # serialFridgeDF = fridgeDF.select("value")
-        # fridgeSchema = StructType([
-        #     StructField("device", StringType(), True),
-        #     StructField("temperature", DoubleType(), True),
-        #     StructField("condition", StringType(), True),
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)])
+        # -----------------------------------------------
+        serialFridgeDF = fridgeDF.select("value")
+        fridgeSchema = StructType([
+            StructField("device", StringType(), True),
+            StructField("temperature", DoubleType(), True),
+            StructField("condition", StringType(), True),
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)])
 
-        # fridgeUDF = udf(createPdu, fridgeSchema)
-        # fridgeDF = serialFridgeDF.select(fridgeUDF("value").alias("fridgeData"))
+        fridgeUDF = udf(createPdu, fridgeSchema)
+        fridgeDF = serialFridgeDF.select(fridgeUDF("value").alias("fridgeData"))
 
-        # fridgeReadyDF = fridgeDF.select(
-        #     fridgeDF.fridgeData.device.alias("device"),
-        #     fridgeDF.fridgeData.temperature.alias("temperature"),
-        #     fridgeDF.fridgeData.condition.alias("condition"),
-        #     fridgeDF.fridgeData.attack.alias("attack"),
-        #     fridgeDF.fridgeData.label.alias("label")
+        fridgeReadyDF = fridgeDF.select(
+            fridgeDF.fridgeData.device.alias("device"),
+            fridgeDF.fridgeData.temperature.alias("temperature"),
+            fridgeDF.fridgeData.condition.alias("condition"),
+            fridgeDF.fridgeData.attack.alias("attack"),
+            fridgeDF.fridgeData.label.alias("label"))
 
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedFridgeDF = fridgeReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedFridgeDF = fridgeReadyDF.withColumn("uuid", uuid_udf())
 
-        # fridgeQuery = expandedFridgeDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        fridgeQuery = expandedFridgeDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # fridgeQuery.awaitTermination()
+        fridgeQuery.awaitTermination()
 
         # ----------------------------------------------- 
         # Process data for the "garage" topic
-        # serialGarageDF = garageDF.select("value")
-        # garageSchema = StructType([
-        #     StructField("door_state", StringType(), True),
-        #     StructField("sphone", IntegerType(), True), 
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)])
+        # -----------------------------------------------
+        serialGarageDF = garageDF.select("value")
+        garageSchema = StructType([
+            StructField("door_state", StringType(), True),
+            StructField("sphone", IntegerType(), True), 
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)])
 
-        # garageUDF = udf(createPdu, garageSchema)
-        # garageDF = serialGarageDF.select(garageUDF("value").alias("garageData"))
+        garageUDF = udf(createPdu, garageSchema)
+        garageDF = serialGarageDF.select(garageUDF("value").alias("garageData"))
 
-        # garageReadyDF = garageDF.select( 
-        #     garageDF.garageData.door_state.alias("door_state"),
-        #     garageDF.garageData.sphone.alias("sphone"),
-        #     garageDF.garageData.attack.alias("attack"),
-        #     garageDF.garageData.label.alias("label"))
+        garageReadyDF = garageDF.select( 
+            garageDF.garageData.door_state.alias("door_state"),
+            garageDF.garageData.sphone.alias("sphone"),
+            garageDF.garageData.attack.alias("attack"),
+            garageDF.garageData.label.alias("label"))
 
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedGarageDF = garageReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedGarageDF = garageReadyDF.withColumn("uuid", uuid_udf())
 
-        # garageQuery = expandedGarageDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        garageQuery = expandedGarageDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # garageQuery.awaitTermination()
+        garageQuery.awaitTermination()
         
         # -----------------------------------------------
         # # Process data for the "gps" topic 
         # serialGpsDF = gpsDF.select("value")
-        # gpsSchema = StructType([
-        #     StructField("entityLocation", StructType([
-        #         StructField("x", FloatType(), True),
-        #         StructField("y", FloatType(), True),
-        #         StructField("z", FloatType(), True)]), True),
-        #     StructField("entityOrientation", StructType([
-        #         StructField("psi", FloatType(), True),
-        #         StructField("theta", FloatType(), True),
-        #         StructField("phi", FloatType(), True)]), True),
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)    
-        #     ])
+        # -----------------------------------------------
+        gpsSchema = StructType([
+            StructField("entityLocation", StructType([
+                StructField("x", FloatType(), True),
+                StructField("y", FloatType(), True),
+                StructField("z", FloatType(), True)]), True),
+            StructField("entityOrientation", StructType([
+                StructField("psi", FloatType(), True),
+                StructField("theta", FloatType(), True),
+                StructField("phi", FloatType(), True)]), True),
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)    
+            ])
 
-        # gpsUDF = udf(createPdu, gpsSchema)
-        # gpsDF = serialGpsDF.select(gpsUDF("value").alias("gpsData")) 
+        gpsUDF = udf(createPdu, gpsSchema)
+        gpsDF = serialGpsDF.select(gpsUDF("value").alias("gpsData")) 
 
-        # gpsReadyDF = gpsDF.select( 
-        #     gpsDF.gpsData.entityLocation.x.alias("longitude"),
-        #     gpsDF.gpsData.entityLocation.y.alias("latitude"),
-        #     gpsDF.gpsData.entityLocation.z.alias("altitude"),
-        #     gpsDF.gpsData.entityOrientation.psi.alias("roll"),
-        #     gpsDF.gpsData.entityOrientation.theta.alias("pitch"),
-        #     gpsDF.gpsData.entityOrientation.phi.alias("yaw"),
-        #     gpsDF.gpsData.attack.alias("attack"),
-        #     gpsDF.gpsData.label.alias("label"))
+        gpsReadyDF = gpsDF.select( 
+            gpsDF.gpsData.entityLocation.x.alias("longitude"),
+            gpsDF.gpsData.entityLocation.y.alias("latitude"),
+            gpsDF.gpsData.entityLocation.z.alias("altitude"),
+            gpsDF.gpsData.entityOrientation.psi.alias("roll"),
+            gpsDF.gpsData.entityOrientation.theta.alias("pitch"),
+            gpsDF.gpsData.entityOrientation.phi.alias("yaw"),
+            gpsDF.gpsData.attack.alias("attack"),
+            gpsDF.gpsData.label.alias("label"))
 
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedGpsDF = gpsReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedGpsDF = gpsReadyDF.withColumn("uuid", uuid_udf())
 
-        # gpsQuery = expandedGpsDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        gpsQuery = expandedGpsDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # gpsQuery.awaitTermination()
+        gpsQuery.awaitTermination()
 
         # -----------------------------------------------
         # Process data for the "light" topic  
-        # serialLightDF = lightDF.select("value")
-        # lightSchema = StructType([
-        #     StructField("motion_status", StringType(), True),
-        #     StructField("light_status", StringType(), True),
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)])
+        # -----------------------------------------------
+        serialLightDF = lightDF.select("value")
+        lightSchema = StructType([
+            StructField("motion_status", StringType(), True),
+            StructField("light_status", StringType(), True),
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)])
         
-        # lightUDF = udf(createPdu, lightSchema)
-        # lightDF = serialLightDF.select(lightUDF("value").alias("lightData"))
+        lightUDF = udf(createPdu, lightSchema)
+        lightDF = serialLightDF.select(lightUDF("value").alias("lightData"))
 
-        # lightReadyDF = lightDF.select(
-        #     lightDF.lightData.motion_status.alias("motion_status"),
-        #     lightDF.lightData.light_status.alias("light_status"),
-        #     lightDF.lightData.attack.alias("attack"),
-        #     lightDF.lightData.label.alias("label"))
+        lightReadyDF = lightDF.select(
+            lightDF.lightData.motion_status.alias("motion_status"),
+            lightDF.lightData.light_status.alias("light_status"),
+            lightDF.lightData.attack.alias("attack"),
+            lightDF.lightData.label.alias("label"))
         
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedLightDF = lightReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedLightDF = lightReadyDF.withColumn("uuid", uuid_udf())
 
-        # lightQuery = expandedLightDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        lightQuery = expandedLightDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # lightQuery.awaitTermination()
+        lightQuery.awaitTermination()
 
         # -----------------------------------------------
         # Process data for the "modbus" topic  
-        # serialModbusDF = modbusDF.select("value")
-        # modbusSchema = StructType([
-        #     StructField("fc1", DoubleType(), True),
-        #     StructField("fc2", DoubleType(), True),
-        #     StructField("fc3", DoubleType(), True),
-        #     StructField("fc4", DoubleType(), True),
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)])
+        # -----------------------------------------------
+        serialModbusDF = modbusDF.select("value")
+        modbusSchema = StructType([
+            StructField("fc1", DoubleType(), True),
+            StructField("fc2", DoubleType(), True),
+            StructField("fc3", DoubleType(), True),
+            StructField("fc4", DoubleType(), True),
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)])
         
-        # modbusUDF = udf(createPdu, modbusSchema)
-        # modbusDF = serialModbusDF.select(modbusUDF("value").alias("modbusData"))
+        modbusUDF = udf(createPdu, modbusSchema)
+        modbusDF = serialModbusDF.select(modbusUDF("value").alias("modbusData"))
 
-        # modbusReadyDF = modbusDF.select(
-        #     modbusDF.modbusData.fc1.alias("fc1"),
-        #     modbusDF.modbusData.fc2.alias("fc2"),
-        #     modbusDF.modbusData.fc3.alias("fc3"),
-        #     modbusDF.modbusData.fc4.alias("fc4"),
-        #     modbusDF.modbusData.attack.alias("attack"),
-        #     modbusDF.modbusData.label.alias("label"))
+        modbusReadyDF = modbusDF.select(
+            modbusDF.modbusData.fc1.alias("fc1"),
+            modbusDF.modbusData.fc2.alias("fc2"),
+            modbusDF.modbusData.fc3.alias("fc3"),
+            modbusDF.modbusData.fc4.alias("fc4"),
+            modbusDF.modbusData.attack.alias("attack"),
+            modbusDF.modbusData.label.alias("label"))
         
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedModbusDF = modbusReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedModbusDF = modbusReadyDF.withColumn("uuid", uuid_udf())
 
-        # modbusQuery = expandedModbusDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        modbusQuery = expandedModbusDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # modbusQuery.awaitTermination()
+        modbusQuery.awaitTermination()
 
         # -----------------------------------------------
-        # Process data for the "thermostat" topic 
-        # serialThermostatDF = thermostatDF.select("value")
-        # thermostatSchema = StructType([
-        #     StructField("device", StringType(), True),
-        #     StructField("temperature", DoubleType(), True),
-        #     StructField("temp_status", IntegerType(), True),
-        #     StructField("attack", StringType(), True),
-        #     StructField("label", IntegerType(), True)])
+        # Process data for the "thermostat" topic
+        # ----------------------------------------------- 
+        serialThermostatDF = thermostatDF.select("value")
+        thermostatSchema = StructType([
+            StructField("device", StringType(), True),
+            StructField("temperature", DoubleType(), True),
+            StructField("temp_status", IntegerType(), True),
+            StructField("attack", StringType(), True),
+            StructField("label", IntegerType(), True)])
         
-        # thermostatUDF = udf(createPdu, thermostatSchema)
-        # thermostatDF = serialThermostatDF.select(thermostatUDF("value").alias("thermostatData"))
+        thermostatUDF = udf(createPdu, thermostatSchema)
+        thermostatDF = serialThermostatDF.select(thermostatUDF("value").alias("thermostatData"))
 
-        # thermostatReadyDF = thermostatDF.select(
-        #     thermostatDF.thermostatData.device.alias("device"),
-        #     thermostatDF.thermostatData.temperature.alias("temperature"),
-        #     thermostatDF.thermostatData.temp_status.alias("temp_status"),
-        #     thermostatDF.thermostatData.attack.alias("attack"),
-        #     thermostatDF.thermostatData.label.alias("label"))
+        thermostatReadyDF = thermostatDF.select(
+            thermostatDF.thermostatData.device.alias("device"),
+            thermostatDF.thermostatData.temperature.alias("temperature"),
+            thermostatDF.thermostatData.temp_status.alias("temp_status"),
+            thermostatDF.thermostatData.attack.alias("attack"),
+            thermostatDF.thermostatData.label.alias("label"))
         
-        # uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
-        # expandedThermostatDF = thermostatReadyDF.withColumn("uuid", uuid_udf())
+        uuid_udf = udf(lambda: str(uuid.uuid4()), StringType()).asNondeterministic()
+        expandedThermostatDF = thermostatReadyDF.withColumn("uuid", uuid_udf())
 
-        # thermostatQuery = expandedThermostatDF.writeStream \
-        #     .outputMode("append") \
-        #     .format("console") \
-        #     .option("truncate", False) \
-        #     .start()
+        thermostatQuery = expandedThermostatDF.writeStream \
+            .outputMode("append") \
+            .format("console") \
+            .option("truncate", False) \
+            .start()
         
-        # thermostatQuery.awaitTermination()
+        thermostatQuery.awaitTermination()
 
         # -----------------------------------------------
         # Process data for the "weather" topic 
+        # -----------------------------------------------
         serialWeatherDF = weatherDF.select("value")
         weatherSchema = StructType([
             StructField("device", StringType(), True),
@@ -290,7 +297,7 @@ class SparkStructuredStreaming:
             .start()
         
         weatherQuery.awaitTermination()
-        
+
 
         # # Define your sink operations
         # cassandra_sink = self.save_to_cassandra(fridgeDF)
