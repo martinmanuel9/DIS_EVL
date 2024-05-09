4848e744137f8f4cc963916e9a967894abc94beb
martinmanuel9 <martinmlopez@arizona.edu>
2024-05-09 14:06:51 -0700

    updates to decode

--- evl_streaming_src/dataOps/jitc_datagen.py
+++ evl_streaming_src/dataOps/jitc_datagen.py
@@ -43,11 +43,27 @@ import base64
 class JITC_DATAOPS:
     def __init__(self, dataset):
         self.dataset = dataset
-        self.data = []  # List to store processed data
+        self.data = {}  # dict to store processed data
+        self.dataframe = pd.DataFrame()
         self.import_data()
 
+
+    ## Step 1: Get all json files from onedrive and place them in a single directory 
+    ## Step 2: Run this once once you download all files and put them in a single directory 
+    def update_jsons(self, directory):
+        for filename in os.listdir(directory):
+            if filename.endswith('.json'):
+                #prepend and append {} of each file 
+                with open(directory + '/' + filename, 'r') as f:
+                    data = f.read()
+                    data = '{\"binary\":' + data + '}'
+                with open(directory + '/' + filename, 'w') as f:
+                    f.write(data)
+
+    # add binaries into a list
     def process_directory(self, directory):
         # print("Processing directory:", directory)
+        # need to prepend each file with {} to make json file correct
         for filename in os.listdir(directory):
             if filename.endswith('.json'):
                 filepath = os.path.join(directory, filename)
@@ -56,36 +72,51 @@ class JITC_DATAOPS:
     def process_json_file(self, json_file):
         with open(json_file, 'r') as f:
             json_data = json.load(f)  # Load JSON data
-            # remove quotes 
-            json_data = json_data.replace("\"", "")
-            self.data.append({'filename': os.path.basename(json_file), 'binary': json_data})
+            # print(json_data['binary'])
+            self.data[os.path.basename(json_file)] = json_data['binary']
         
     def import_data(self):
         self.process_directory(os.getcwd() + '/data/JITC_Data')
-        self.data = pd.DataFrame(self.data)
+        self.dataframe = pd.DataFrame.from_dict(self.data, orient='index', columns=['binary'])
+        self.dataframe.index.name = 'filename'
         
     def decode_dataset(self, data):
-        X = data.to_frame()
+        X = data
         # go through dataframe and decode binary data
         for i in range(len(X)):
-            print('Before:\n',X['binary'].iloc[i][0])
-            X['binary'].iloc[i] = binascii.b2a_base64(bitarray(X['binary'].iloc[i]), newline=True).decode()
-            decodedBytes = base64.b64decode(X['binary'].iloc[i])
-            X['binary'].iloc[i] = decodedBytes.decode('ascii')
-            
-        
-        print('After:\n', X)
+            # print('Before:\n', type(X['binary'].iloc[i]))
+            binary_str = X['binary'].iloc[i]
+            # convert to bytes
+            bytes_list = [binary_str[i:i+8] for i in range(0, len(binary_str), 8)]
+            byte_values = [int(byte, 2) for byte in bytes_list]
+            decoded_values = [bytes([byte]).decode('utf-8') for byte in byte_values]
+            print('Decoded:', decoded_values)
         return X
     
 
     def develop_dataset(self, ):
-        X_train, X_test = train_test_split(self.data['binary'], test_size=0.8, random_state=42)
+        X_train, X_test = train_test_split(self.dataframe, test_size=0.8, random_state=42)
         
-        # X_train_decoded = self.decode_dataset(X_train)
+        X_train_decoded = self.decode_dataset(X_train)
+        
+        
+        # X_train = pd.DataFrame(X_train)
+        # # Find duplicate rows based on the 'binary' column
+        # duplicates = X_train.duplicated(subset=['binary'], keep=False)
+        
+        # X_train['label'] = ~duplicates
+
+        # # Print instances where label is True
+        # print(X_train[X_train['label'] == True])
+        
+        
+
 
         return X_train, X_test
 
-# if __name__ == "__main__":
-#     dataOps = JITC_DATAOPS(dataset='JITC')
-#     X_train, X_test= dataOps.develop_dataset()
-#     print(X_train)
+if __name__ == "__main__":
+    dataOps = JITC_DATAOPS(dataset='JITC')
+    # run the following only once to update json files
+    # dataOps.update_jsons(os.getcwd() + '/data/JITC_Data')
+    X_train, X_test= dataOps.develop_dataset()
+
